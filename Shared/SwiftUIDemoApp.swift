883b243af2e62e216c93e2edff24d0bbf40b9d86
claus <cshagen@chlaus.de>
2021-09-07 13:00:34 +0200

    Initial Commit

---
+++ Shared/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  Shared
+//
+//  Created by claus on 07.09.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
